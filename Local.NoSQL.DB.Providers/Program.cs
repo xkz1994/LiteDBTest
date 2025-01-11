b978fc8882711feff44e1c7e3d015bce5993056e
Kaizhi Xuan <xuankaizhi@semixchina.com>
2025-01-11 15:36:24 +0800

    测试性能

--- Local.NoSQL.DB.Providers/Program.cs
+++ Local.NoSQL.DB.Providers/Program.cs
@@ -1,4 +1,5 @@
-﻿using Local.NoSQL.DB.Providers;
+﻿using System.Diagnostics;
+using Local.NoSQL.DB.Providers;
 using Local.NoSQL.DB.Providers.Implements;
 using Local.NoSQL.DB.Providers.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,17 +19,39 @@ var serviceProvider = host.Services;
 
 var cacheProvider = serviceProvider.GetRequiredService<ICacheProvider>();
 
-var tests = cacheProvider.GetArray<Test>(CancellationToken.None);
-Console.WriteLine(LiteDbCacheProviderImpl._useLiteDbCount);
+var timestamp = Stopwatch.GetTimestamp();
 
-Test[] insert = [.. Enumerable.Range(1, 10).Select(i => new Test { TestId = i }).ToArray()];
-var set = cacheProvider.SetArray(insert, CancellationToken.None);
-Console.WriteLine(LiteDbCacheProviderImpl._useLiteDbCount);
-Console.WriteLine(set);
+var taskList = new List<Task>();
+for (var i = 0; i < 10; i++)
+{
+    taskList.Add(new Task(() =>
+    {
+        try
+        {
+            var tests = cacheProvider.GetArray<Test>(CancellationToken.None);
+            Console.WriteLine(LiteDbCacheProviderImpl._useLiteDbCount);
 
-tests = cacheProvider.GetArray<Test>(CancellationToken.None);
-Console.WriteLine(LiteDbCacheProviderImpl._useLiteDbCount);
+            Test[] insert = [.. Enumerable.Range(1, 10).Select(i => new Test { TestId = i }).ToArray()];
+            var set = cacheProvider.SetArray(insert, CancellationToken.None);
+            Console.WriteLine(LiteDbCacheProviderImpl._useLiteDbCount);
+            Console.WriteLine(set);
 
+            tests = cacheProvider.GetArray<Test>(CancellationToken.None);
+            Console.WriteLine(LiteDbCacheProviderImpl._useLiteDbCount);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }));
+}
+
+// taskList执行
+taskList.ForEach(t => t.Start());
+
+Task.WaitAll(taskList.ToArray());
+
+Console.WriteLine(Stopwatch.GetElapsedTime(timestamp).TotalSeconds);
 
 cacheProvider.Dispose();
 Console.WriteLine("===========================");
